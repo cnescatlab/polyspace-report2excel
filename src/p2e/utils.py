81303e8cd12a060181d024af5ad71becd191fb42
Diego Rodriguez <diego.rodriguez@cnes.fr>
2023-04-19 12:19:47 +0000

    Enhance stringtofilename method

--- src/p2e/utils.py
+++ src/p2e/utils.py
@@ -1,15 +1,18 @@
+import os, string
+
 class Utils:
 
+    " Enleve tout les caracteres speciaux de la chaine value."
     @staticmethod
     def normalize(value):
-        """ Enleve tout les caracteres speciaux de la chaine value """
         value = value.strip()
         value = "".join(x for x in value if x.isalnum() or x == " ")
         return value
 
+    " Recupere le nom d'un fichier."
     @staticmethod
     def stringtofilename(value):
-        """ Recupere le nom d'un fichier. On suppose que ce nom ne contient pas d'espace. """
-        value = "".join(x for x in value if x not in "[]:*?/")
-        value = value.replace(" ", "\\")
-        return value.split("\\")[-1]
+        valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
+        basename = os.path.basename(value)
+        cleaned_basename = ''.join(c for c in basename if c in valid_chars)
+        return cleaned_basename
